49a31835099995fc0592c7a805421b1c478a5baf
Jonas Herrmannsdörfer <jonas@herrmannsdoerfer.dev>
2023-04-16 15:20:44 +0200

    mouse pr added and types fixed

--- app/drivers/gpio/gpio_max7318.c
+++ app/drivers/gpio/gpio_max7318.c
@@ -12,8 +12,8 @@
 
 #include <errno.h>
 
-#include <kernel.h>
-#include <device.h>
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
 #include <init.h>
 #include <sys/byteorder.h>
 #include <drivers/gpio.h>
@@ -21,7 +21,7 @@
 #include <drivers/ext_power.h>
 
 #define LOG_LEVEL CONFIG_GPIO_LOG_LEVEL
-#include <logging/log.h>
+#include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(gpio_max7318);
 
